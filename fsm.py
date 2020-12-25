49f96dcfa2e1dcb2edf69ed984ee38603c98579d
ant11029 <cchs11029@gmail.com>
2020-12-25 23:11:54 +0800

    final

--- fsm.py
+++ fsm.py
@@ -1,6 +1,6 @@
 from transitions.extensions import GraphMachine
 
-from utils import send_text_message
+from utils import send_text_message, send_image_url
 
 import random
 
@@ -15,7 +15,7 @@ class TocMachine(GraphMachine):
 
     def is_going_to_state2(self, event):
         text = event.message.text
-        return text.lower() == "go to state2"
+        return text.lower() == "fsm"
 
     def on_enter_state1(self, event):
         print("I'm entering state1")
@@ -37,7 +37,7 @@ class TocMachine(GraphMachine):
         print("I'm entering state2")
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state2")
+        send_image_url(reply_token, 'https://i.imgur.com/6Hpy75t.png')
         self.go_back()
 
     def on_exit_state2(self):
